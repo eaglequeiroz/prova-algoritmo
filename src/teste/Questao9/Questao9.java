23e431f16541cf55a6f6bd252e53aaa91b34fce3
Igor Queiroz <igorcenourox@gmail.com>
2017-11-23 17:45:04 -0300

    Refatorando Questão 9

--- src/teste/Questao9/Questao9.java
+++ src/teste/Questao9/Questao9.java
@@ -1,33 +1,50 @@
 package teste.Questao9;
 
+/**
+ *
+ * @author Igor Queiroz
+ */
 public class Questao9 {
-	
-	public static Integer sumOfTreeChildrenNodes(BinnaryTree b) {
-		
-		if (b != null) {
-			if (b.left != null) { b.valor += sumOfTreeChildrenNodes(b.left); }
-			if (b.right != null) { b.valor += sumOfTreeChildrenNodes(b.right); }
-			return b.valor;
-		}
-		
-		return null;
-	}
-	
-	
-	/*
-	 * Dados para fins de teste do conceito.
-	 */
-	public static void main(String[] args) {
-		BinnaryTree tree = new BinnaryTree();
-		tree.valor = 1;
-		tree.right = new BinnaryTree();
-		tree.left = new BinnaryTree();
-		tree.right.valor = 5;
-		tree.left.valor = 7;
-		tree.left.left = new BinnaryTree();
-		tree.left.left.valor = 5;
-		
-		System.out.println(Questao9.sumOfTreeChildrenNodes(tree.left));
-	}
 
+    public static void main(String[] args) {
+        final BinnaryTree no2 = new BinnaryTree(7);
+        final BinnaryTree no4 = new BinnaryTree(5);
+        final BinnaryTree no3 = new BinnaryTree(5, no4);
+        final BinnaryTree no1 = new BinnaryTree(1, no2, no3);
+
+        System.out.println("Soma dos nós filhos: " + no1.somaDosNosFilhos());
+    }
+
+    private static class BinnaryTree {
+
+        private final int valor;
+        private BinnaryTree left;
+        private BinnaryTree right;
+
+        public BinnaryTree(int valor) {
+            this.valor = valor;
+        }
+
+        public BinnaryTree(int valor, BinnaryTree left) {
+            this.valor = valor;
+            this.left = left;
+        }
+
+        public BinnaryTree(int valor, BinnaryTree left, BinnaryTree right) {
+            this.valor = valor;
+            this.left = left;
+            this.right = right;
+        }
+
+        public int somaDosNosFilhos() {
+            int valorAcc = valor;
+            if (left != null) {
+                valorAcc += left.somaDosNosFilhos();
+            }
+            if (right != null) {
+                valorAcc += right.somaDosNosFilhos();
+            }
+            return valorAcc;
+        }
+    }
 }
